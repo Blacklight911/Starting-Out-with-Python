54a65de6b25fd5aa3a2e8749891a45c97cd60872
Blacklight911 <dmitreykyrchin@gmail.com>
2022-11-27 01:48:42 +0300

    This is book: Autor Toni Gaddis. Name: Starting Out with Python 5th edition

---
+++ lessons_2.py
@@ -0,0 +1,8 @@
+"""Lesson 2"""
+# second_name = input('Введите фамилию: ')
+# week_sells = float(input('Введите объем продаж за неделю: '))
+
+print('Black\nwatch')
+print('Black\twatch')
+print('Black\"watch')
+print('Black\'watch\\')

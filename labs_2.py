3ff70aa56131297265e4603c01be834f669ef9b9
Blacklight <dmitreykyrchin@gmail.com>
2022-11-30 16:12:03 +0300

    started labs_2.py

---
+++ labs_2.py
@@ -0,0 +1,111 @@
+"""Алгоритмический тренажер"""
+import turtle
+
+# height = input('Введите свой рост')
+# color = input('Введите свой любимый цвет')
+#
+# a, b, c = 0, 0, 0
+#
+# b = a + 2
+# a = b * 4
+# b = a / 3.14
+# a = b - 8
+#
+# x, w, y, z = 4, 5, 8, 2
+
+# total = 10 + 14
+# down_payment = 10
+# due = total - down_payment
+# subtotal = 15000
+# total = subtotal * 0.15
+# sales = 1500.36910
+# print(f'{sales:.2f}')
+
+# number = 1234567.456
+# print(f'{number:,.1f}')
+
+
+# turtle.forward(100)
+# turtle.setheading(90)
+# turtle.forward(100)
+# turtle.setheading(180)
+# turtle.forward(100)
+# turtle.setheading(270)
+# turtle.forward(100)
+# turtle.penup()
+# turtle.setheading(0)
+# turtle.forward(90)
+# turtle.setheading(90)
+# turtle.forward(50)
+# turtle.fillcolor('red')
+# turtle.begin_fill()
+# turtle.circle(40)
+# turtle.end_fill()
+# turtle.done()
+
+# print('Дмитрий')
+# print('Белгород, улица Некрасова 7, индекс 13378')
+# print('89978861974')
+# print('Информационные Технологии')
+
+# Прогноз продаж.
+# total_sum = int(input('Введите плановую сумму объема продаж '))
+# print(int(total_sum * 0.23))
+
+# Расчет площади земельного участка
+# land_plot = int(input('Введите общее количество квадратных метров земельного участка '))
+# print(land_plot / 4047)
+
+# Общий объём продаж
+# TAX = 0.07
+# all_tax = 0
+# all_price = 0
+# for i in range(5):
+#     price = int(input('Введите цену товара '))
+#     all_price += price
+#     all_tax += price * TAX
+# print('Сумма товара', all_price)
+# print('Налог составил', all_tax)
+# print('Итоговая стоимость составила', all_price + all_tax)
+
+# Пройденное расстояние
+# print('Расстояние, которое автомобиль пройдет за 6 часов:', 70 * 6)
+# print('Расстояние, которое автомобиль пройдет за 10 часов:', 70 * 10)
+# print('Расстояние, которое автомобиль пройдет за 15 часов:', 70 * 15)
+
+# buy_value = int(input('Введите величину покупки: '))
+# federal_tax = 0.05
+# regional_tax = 0.025
+# sum_federal_tax = buy_value * federal_tax
+# sum_regional_tax = buy_value * regional_tax
+# print('Сумма покупки: ', buy_value)
+# print('Федеральный налог составил:', sum_federal_tax)
+# print('Региональный налог составил:', sum_regional_tax)
+# print('Сумма покупки с налогом составила:', buy_value + sum_regional_tax + sum_federal_tax)
+
+# Расход бензина в расчете на километры пройденного пути.
+# kilometers_covered = int(input('Количество пройденных километров'))
+# fuel_on_kilo = int(input('Введите расход бензина в литрах'))
+# print('Расход ', kilometers_covered / fuel_on_kilo)
+
+# Чаевые налог и общая сумма
+# food_cost = int(input('Стоимость еды: '))
+# print('Общая стоимость:', food_cost + food_cost * 0.18 + food_cost * 0.07)
+
+# Преобразователь температуры
+# num = int(input('Введите температуру: '))
+# print('Температура в Фаренгейтах:', 9 / 5 * num + 32)
+
+# # Регулятор ингридиентов. ?
+# how_much_buns = int(input('Какое количество булочек вы хотите испечь? '))
+# print('Стакана сахара', 1.5 / how_much_buns)
+# print('Стакана масла', 1 / how_much_buns)
+# print('Стакана муки', 2.75 / how_much_buns)
+
+# Процент учащихся обоего пола.
+# how_much_man = int(input('Количество учащихся юношей: '))
+# how_much_woman = int(input('Количество учащихся девушек: '))
+# how_much_all = how_much_man + how_much_woman
+# print(f'Учащихся мужского пола {how_much_man / how_much_all:.0%}. Учащихся женского пола {how_much_woman / how_much_all:.0%}')
+
+# Программа расчета купли-продажи акции.
